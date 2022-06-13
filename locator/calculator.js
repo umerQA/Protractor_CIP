3f275817499eb6182d7d2fa727d857ae38f15417
ICMuhammad.Farooq <icmuhammad.farooq@americanwell.com>
2022-06-13 17:38:10 +0500

    first CIP commit

---
+++ locator/calculator.js
@@ -0,0 +1,21 @@
+const CalculatorPage = function() {
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var buttonGo = element(by.id('gobutton'));
+    var result = element(by.binding('latest'))
+
+     this.enterFirstNumber = function(firstNo){
+        firstNumber.sendKeys(firstNo);
+     }
+     this.enterSecondNumber = function(secondNo){
+        secondNumber.sendKeys(secondNo);
+     }
+     this.buttonGo = function(){
+        buttonGo.click();
+     }
+     this.verifyResult = function(result){
+      let output =  element(by.cssContainingText('.ng-binding', result));
+      expect(output.getText()).toEqual(result);
+  }
+};
+module.exports = new CalculatorPage;
---
+++ tests/calculator.js
@@ -0,0 +1,26 @@
+const { before } = require('lodash');
+let CalculatorPage = require('../locator/calculator');
+
+describe('calculator tests', function () {
+
+    beforeEach(function() {
+        browser.get(browser.baseUrl);
+
+    });
+
+    it('addition test', function () {
+        CalculatorPage.enterFirstNumber('4');
+        CalculatorPage.enterSecondNumber('3');
+        CalculatorPage.buttonGo();
+        CalculatorPage.verifyResult('7');
+
+    });
+
+    it('subtraction test', function () {
+        CalculatorPage.enterFirstNumber('4');
+        CalculatorPage.enterSecondNumber('3');
+        CalculatorPage.buttonGo();
+        CalculatorPage.verifyResult('5');
+
+    }); 
+});
